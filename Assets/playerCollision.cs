2f4a869db71025872515f54722ed9900344d9d63
rasmusingelin <brain001@edu.xamk.fi>
2019-11-28 23:51:22 +0200

    Initial commit

---
+++ Assets/playerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour { 
+    public playerMovement movement;
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+    if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        } 
+    }
+}
